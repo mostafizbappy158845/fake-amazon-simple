b6eb0716e6569cf04fdf844726d66b495b43142a
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2022-08-07 20:33:39 +0600

    style-productInfo&btnCart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    // console.log(props.product);
-    const {name,img,seller,rating,price} = props.product;
+    console.log(props.product);
+    const {name,img,seller,ratings,price} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings} starts</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to cart</p>
+            </button>
         </div>
     );
 };
